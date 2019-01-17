cafc6972d95b5eae992868364e9af99b74b98f06
Gabriel Margoses <gabrielmargoses@gmail.com>
2019-01-18 00:50:52 +0100

    layouts updated

--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,10 @@ const router = express.Router();
 const products = [];
 
 router.get("/add-product", (req, res, next) => {
-  res.render("add-product", { pageTitle: "Add Product" });
+  res.render("add-product", {
+    pageTitle: "Add Product 💈",
+    path: "/admin/add-product"
+  });
 });
 
 router.post("/add-product", (req, res, next) => {
