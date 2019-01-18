337e6d2362879f8fb09d47457436482f353aeb29
Gabriel Margoses <gabrielmargoses@gmail.com>
2019-01-19 00:36:57 +0100

    controllers finished

--- controllers/products.js
+++ controllers/products.js
@@ -1,25 +1,25 @@
 const products = [];
 
 exports.getAddProduct = (req, res, next) => {
-  res.render("add-product", {
-    pageTitle: "Add Product 💈",
-    path: "/admin/add-product",
-    productCSS: true,
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
     formsCSS: true,
+    productCSS: true,
     activeAddProduct: true
   });
 };
 
 exports.postAddProduct = (req, res, next) => {
   products.push({ title: req.body.title });
-  res.redirect("/");
+  res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
-  res.render("shop", {
+  res.render('shop', {
     prods: products,
-    pageTitle: "Shop 🛒",
-    path: "/",
+    pageTitle: 'Shop',
+    path: '/',
     hasProducts: products.length > 0,
     activeShop: true,
     productCSS: true
