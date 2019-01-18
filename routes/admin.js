905d55120cd6e8a6f0df8e5b4d50555dd68d20cb
Gabriel Margoses <gabrielmargoses@gmail.com>
2019-01-18 02:01:53 +0100

    layouts converted to handlebars

--- routes/admin.js
+++ routes/admin.js
@@ -11,7 +11,10 @@ const products = [];
 router.get("/add-product", (req, res, next) => {
   res.render("add-product", {
     pageTitle: "Add Product 💈",
-    path: "/admin/add-product"
+    path: "/admin/add-product",
+    productCSS: true,
+    formsCSS: true,
+    activeAddProduct: true
   });
 });
 
