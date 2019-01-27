cc6ace289f7de4391ea7d2f6905c94089638d0a4
Gabriel Margoses <gabrielmargoses@gmail.com>
2019-01-27 03:43:00 +0100

    code improvement

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,8 +4,7 @@ exports.getAddProduct = (req, res, next) => {
   res.render("admin/edit-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
-    editing: false,
-    isAuthenticated: req.session.isLoggedIn
+    editing: false
   });
 };
 
@@ -48,8 +47,7 @@ exports.getEditProduct = (req, res, next) => {
         pageTitle: "Edit Product",
         path: "/admin/edit-product",
         editing: editMode,
-        product: product,
-        isAuthenticated: req.session.isLoggedIn
+        product: product
       });
     })
     .catch(err => console.log(err));
@@ -71,7 +69,7 @@ exports.postEditProduct = (req, res, next) => {
       return product.save();
     })
     .then(result => {
-      console.log("UPDATED PRODUCT!");
+      console.log("Updated Product");
       res.redirect("/admin/products");
     })
     .catch(err => console.log(err));
@@ -86,8 +84,7 @@ exports.getProducts = (req, res, next) => {
       res.render("admin/products", {
         prods: products,
         pageTitle: "Admin Products",
-        path: "/admin/products",
-        isAuthenticated: req.session.isLoggedIn
+        path: "/admin/products"
       });
     })
     .catch(err => console.log(err));
@@ -97,7 +94,7 @@ exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   Product.findByIdAndRemove(prodId)
     .then(() => {
-      console.log("DESTROYED PRODUCT");
+      console.log("Destroyed Product");
       res.redirect("/admin/products");
     })
     .catch(err => console.log(err));
